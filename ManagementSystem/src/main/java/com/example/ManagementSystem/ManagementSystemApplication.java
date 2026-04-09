b4674fc4eb89b6b55f2524aedc2b4a219ea2b4f8
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2026-04-09 15:35:33 -0300

    WIP: Home and Project Config

--- ManagementSystem/src/main/java/com/example/ManagementSystem/ManagementSystemApplication.java
+++ ManagementSystem/src/main/java/com/example/ManagementSystem/ManagementSystemApplication.java
@@ -2,12 +2,11 @@ package com.example.ManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementSystemApplication.class, args);
 	}
-
 }
