3b11734ea7c9e8cf8454ca45ec883fda59f239bb
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2026-04-13 12:29:57 -0300

    WIP: User Domain

--- ManagementSystem/src/main/resources/db/migration/V1__init.sql
+++ ManagementSystem/src/main/resources/db/migration/V1__init.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
                           name VARCHAR(100) NOT NULL,
                           email VARCHAR(100) UNIQUE NOT NULL,
                           password VARCHAR(255) NOT NULL,
-                          profile VARCHAR(20) NOT NULL -- ADMIN, COLABORADOR, MORADOR
+                          profile VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE blocks (
